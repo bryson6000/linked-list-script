04ea3765a2dc2773bcb8f3f1c0fee7d0bdf3e065
bryson6000 <168474935+bryson6000@users.noreply.github.com>
2025-05-09 11:54:39 +0300

    test.py

--- template.py
+++ template.py
@@ -1,17 +1,7 @@
-# DO NOT MODIFY THIS
 from linkedLists import *
 
-
-# Function to test goes here
-
-
-
-# Create the Linked List here and invoke the function
-# Check the README for more details
 def main():
     pass
 
-
-# Use this always. Allows main function to work correctly
 if __name__ == '__main__':
     main()
